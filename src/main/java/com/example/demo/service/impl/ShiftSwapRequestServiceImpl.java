c1c3f65747332a47385a0a80490de98b6b05600a
juang0615 <jms850615@gamil.com>
2025-06-11 17:00:20 +0800

    HHHHELP

--- src/main/java/com/example/demo/service/impl/ShiftSwapRequestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ShiftSwapRequestServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.entity.User;
 import com.example.demo.repository.ScheduleRepository;
 import com.example.demo.repository.ShiftSwapRequestRepository;
 import com.example.demo.service.NotificationService;
@@ -19,6 +20,9 @@ public class ShiftSwapRequestServiceImpl implements ShiftSwapRequestService {
 	
 	@Autowired
 	private NotificationService notificationService;
+	
+	@Override
+	public ShiftSwapRequest requestSwap(User requester, User target)
 	
 	
 
