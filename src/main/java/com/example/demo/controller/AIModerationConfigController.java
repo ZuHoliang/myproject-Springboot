3f22ecca1909317524c1522f1e0f55877f1bae74
juang0615 <jms850615@gamil.com>
2025-06-19 12:18:44 +0800

    controller

--- src/main/java/com/example/demo/controller/AIModerationConfigController.java
+++ src/main/java/com/example/demo/controller/AIModerationConfigController.java
@@ -1,15 +1,21 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.config.AIModerationConfig;
 import com.example.demo.service.AuthService;
 
 import jakarta.servlet.http.HttpSession;
 
+@RestController
+@RequestMapping("/api/admin/moderation")
+@CrossOrigin(origins = { "http://localhost:5173", "http://localhost:8008" }, allowCredentials = "true")
 public class AIModerationConfigController {
 	
 	@Autowired
